08346466bbe70d1b96e76bd286d240ba71ce1012
bogdanmagometa <bogdan.magometa@gmail.com>
2021-02-06 14:12:16 +0200

    Implemented validate_board, check_uniqueness_in_rows and check_row, created check_uniqueness_in_cols

--- puzzle.py
+++ puzzle.py
@@ -22,6 +22,74 @@ def validate_board(board: list):
     False
     """
 
+    return check_uniqueness_in_rows(board) and check_uniqueness_in_cols(board) and \
+                                                                    check_uniqueness_colors(board)
+
+
+def check_uniqueness_in_rows(board: list):
+    """
+    Check if all the numbers in each row are unique within that row.
+
+    >>> board = ["**** ****",
+    ... "***1 ****",
+    ... "**  3****",
+    ... "* 4 1****",
+    ... "     9 5 ",
+    ... " 6  83  *",
+    ... "3   1  **",
+    ... "  8  2***",
+    ... "  2  ****"]
+    >>> check_uniqueness_in_rows(board)
+    True
+    >>> board = [
+    ... '12',
+    ... '33']
+    >>> check_uniqueness_in_rows(board)
+    False
+    >>> board = [
+    ... '1**',
+    ... '12*',
+    ... '123']
+    >>> check_uniqueness_in_rows(board)
+    True
+    """
+
+    for row in board:
+        if not check_row(row):
+            return False
+
+    return True
+
+
+def check_row(row: str):
+    """
+    Check if all the digits, which are greater than 0, in row are unique.
+
+    >>> check_row('3132')
+    False
+    >>> check_row('****')
+    True
+    >>> check_row('9')
+    True
+    >>> check_row('66')
+    False
+    >>> check_row('563784192')
+    True
+    """
+
+    nums_set = set()
+
+    for char in row:
+        if char.isdigit() and char != '0':
+            if char in nums_set:
+                return False
+            nums_set.add(char)
+
+    return True
+
+
+def check_uniqueness_in_cols(board: list):
+
     return False
 
 
