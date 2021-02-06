9a93a932c40c37672a3113802148d337cdc56279
bogdanmagometa <bogdan.magometa@gmail.com>
2021-02-06 15:38:35 +0200

    Implemented check_uniqueness_colors and mentioned github repository in documentation of puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -2,6 +2,9 @@
 puzzle.py
 
 Module for checking if the board is ready.
+
+Github repository:
+https://github.com/bogdanmagometa/puzzle
 """
 
 def validate_board(board: list):
@@ -145,6 +148,44 @@ def transpose_board(board: list):
             transposed_board[-1] += column[row]
 
     return transposed_board
+
+
+def check_uniqueness_colors(board: list):
+    """
+    Check if the numbers of each color are unique withing that color.
+    Return True if unique, False otherwise.
+
+    >>> board = ["**** ****",
+    ... "***1 ****",
+    ... "**  3****",
+    ... "* 4 1****",
+    ... "     9 5 ",
+    ... " 6  83  *",
+    ... "3   1  **",
+    ... "  8  2***",
+    ... "  2  ****"]
+    >>> check_uniqueness_colors(board)
+    True
+    """
+
+    for pivot in zip(range(len(board)-1, -1, -1), range(len(board))):
+        nums_set = set()
+        for row_idx in range(pivot[0]):
+            cur_num = board[row_idx][pivot[1]]
+            if cur_num.isdigit() and cur_num != '0':
+                if cur_num in nums_set:
+                    return False
+                nums_set.add(cur_num)
+
+        for col_idx in range(pivot[1], len(board)):
+            cur_num = board[pivot[0]][col_idx]
+            if cur_num.isdigit() and cur_num != '0':
+                if cur_num in nums_set:
+                    return False
+                nums_set.add(cur_num)
+
+    return True
+
 
 if __name__ == '__main__':
     import doctest
